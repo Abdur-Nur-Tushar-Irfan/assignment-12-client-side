0cf27eb2f670638cc2e9de6354e91d35b594c664
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-11-25 00:13:46 +0600

    bookings added

--- src/Pages/Home/Home/BookNow/BookNow.js
+++ src/Pages/Home/Home/BookNow/BookNow.js
@@ -1,9 +1,48 @@
 import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
 import { AuthContext } from '../../../Context/UserContext';
 
-const BookNow = ({selectCategory}) => {
-    const {category,name,resalePrice}=selectCategory
+const BookNow = ({selectCategory,setSelectCategory}) => {
+    const {category,name,resalePrice,orginialPrice}=selectCategory
     const {user}=useContext(AuthContext)
+    const handlebooking=(e)=>{
+        e.preventDefault();
+        console.log('click')
+        const form=e.target;
+        const email=form.email.value;
+        const category=form.category.value;
+        const resalePrice=form.resalePrice.value;
+        const orginialPrice=form.orginialPrice.value;
+        const location=form.location.value;
+        const phone=form.phone.value;
+        form.reset()
+        const bookings={
+            userName:user?.displayName,
+            productName:category,
+            email,
+            phone,
+            orginialPrice,
+            resalePrice,
+            location
+        }
+        console.log(bookings)
+        fetch('http://localhost:5000/bookings',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(bookings)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.acknowledged>0){
+                console.log(data)
+                toast.success('Booking Confirmed')
+                setSelectCategory(null)
+            }
+        })
+
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -11,19 +50,20 @@ const BookNow = ({selectCategory}) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold"></h3>
-                    <form className='mt-6'>
-                        <input type="text" defaultValue={user?.displayName}  className="input input-bordered w-full mb-2 " />
-                       
-                        <input type="text" name='patientName'  defaultValue={user?.email}  placeholder='Name' className="input input-bordered w-full mb-2" />
-                        <input type="email" name='email' value={category}  placeholder='your email' className="input input-bordered w-full mb-2" />
-                        <input type="phone" name='phone' value={resalePrice} placeholder='Your Number' className="input input-bordered w-full mb-2" />
+                    <form onSubmit={handlebooking} className='mt-6'>
+                        <h1 className='font-bold mb-4'>{name}</h1>
+                        <input type="text" name='name'  defaultValue={user?.displayName}  placeholder='Name' className="input input-bordered w-full mb-2" />
+                        <input type="email" name='email'  defaultValue={user?.email}  placeholder='Name' className="input input-bordered w-full mb-2" />
+                        <input type="text" name='category' value={category}  placeholder='your email' className="input input-bordered w-full mb-2" />
+                        <input type="text" name='resalePrice' value={resalePrice} placeholder='Your Number' className="input input-bordered w-full mb-2" />
+                        <input type="text" name='orginialPrice' value={orginialPrice} placeholder='Your Number' className="input input-bordered w-full mb-2" />
+                        <input type="text" name='location'  placeholder='Your Location' className="input input-bordered w-full mb-2" />
+                        <input type="phone" name='phone'  placeholder='Your Number' className="input input-bordered w-full mb-2" />
                         <input type="submit" value='Submit' placeholder="Type here" className="input bg-accent input-bordered w-full font-bold " />
 
                     </form>
                 </div>
             </div>
-
-
         </>
     );
 };
--- src/Pages/Home/SingleCategories/SingleCategories.js
+++ src/Pages/Home/SingleCategories/SingleCategories.js
@@ -5,30 +5,33 @@ import BookNow from '../Home/BookNow/BookNow';
 import SingleCategory from './SingleCategory';
 
 const SingleCategories = () => {
-    const [selectCategory, setSelectCategory] = useState({})
-    const {id}=useParams()
+    const [selectCategory, setSelectCategory] = useState(null)
+    const { id } = useParams()
     console.log(id)
-    const {data:allCategories=[]}=useQuery({
-        queryKey:['allCategories'],
-        queryFn:()=>fetch(`http://localhost:5000/allCategories/${id}`)
-        .then(res=>res.json())
+    const { data: allCategories = [] } = useQuery({
+        queryKey: ['allCategories'],
+        queryFn: () => fetch(`http://localhost:5000/allCategories/${id}`)
+            .then(res => res.json())
     })
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            {  
-                allCategories?.map(allCategory=><SingleCategory 
+            {
+                allCategories?.map(allCategory => <SingleCategory
                     setSelectCategory={setSelectCategory}
                     key={allCategory._id}
                     allCategory={allCategory}
                 >
                 </SingleCategory>)
             }
-             <BookNow
-             selectCategory={selectCategory}
+            {
+                selectCategory &&
+                <BookNow
+                    selectCategory={selectCategory}
+                    setSelectCategory={setSelectCategory}
+                ></BookNow>
+            }
 
-            ></BookNow>
-            
-        </div>
+        </div >
     );
 };
 
